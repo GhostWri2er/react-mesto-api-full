fc8e6f4d9b45eb9718342469ed0913a2f86f6e1f
Ivan Mironovich <ivanmironovich@vk.com>
2023-02-25 22:15:40 +0500

    feat: cors

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -115,10 +115,10 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-40/',
+  baseUrl: 'https://api.mesto.ghostwriter.nomoredomains.work/',
   headers: {
-    authorization: '8a35d6a5-9675-4746-b567-877f8304d5ec',
-    'Content-Type': 'application/json',
+    "Accept": "application/json",
+    "Content-Type": "application/json",
   },
 });
 
