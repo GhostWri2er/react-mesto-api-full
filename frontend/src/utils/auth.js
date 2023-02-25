494b3fa3b929048d6accf1a79dbc25fa86fdbcf0
Ivan Mironovich <ivanmironovich@vk.com>
2023-02-25 23:24:34 +0500

    feat: cors

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -9,7 +9,7 @@ export const register = (data) => {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ email: data.email, password: data.password }),
+    body: JSON.stringify({ "email": data.email, "password": data.password }),
   }).then((res) => checkResponse(res));
 };
 
